33817ba138892f88bf28618b8a4cf79fb2a2e488
Ronald chen <rc9525@gmail.com>
2022-03-27 20:08:13 -0400

    script.js

--- script.js
+++ script.js
@@ -17,8 +17,14 @@ function getCurrentWeatherData (event) {
 
         var currentTemp = document.querySelector("#current-temp");
         currentTemp.textContent = "Temp: " + data.main.temp
-        var currentWind = document.querySelector ("#current-wind");
+        var currentWind = document.querySelector("#current-wind");
         currentWind.textContent = "Wind: " + data.wind.speed
+        var currentHumidity = document.querySelector("#current-humidity");
+        currentHumidity.textContent = "Humidity: " + data.humidity
+        // var currentPrecipitation = document.querySelector("#current-precipitation");
+        // currentPrecipitation.textContent = "Precipitation: " + data.precipitation.mode 
+
+
 
         getForecastData();
     })
@@ -37,11 +43,40 @@ function getForecastData () {
     .then(response => response.json())
     .then(data => {
         console.log(data);
+        var searchedCity = document.querySelector("#searched-city");
+        searchedCity.textContent = cityName;
+
 
 
         // DISPLAY THE DATA
-        console.log(data.list[0].main.temp)
-        console.log(data.list[8].main.temp)
+        var date0 = document.getElementById("date0");
+        date0.textContent = dayjs().add(1, 'day').format("MM/DD/YYYY")
+        var temp0 = document.getElementById("temp0");
+        temp0.textContent = data.list[0].main.temp
+
+        ///////////////
+        var date8 = document.getElementById("date8");
+        date8.textContent = dayjs().add(2, 'day').format("MM/DD/YYYY")
+        var temp8 = document.getElementById("temp8");
+        temp8.textContent = data.list[8].main.temp
+
+        var date16 = document.getElementById("date16");
+        date16.textContent = dayjs().add(3, 'day').format("MM/DD/YYYY")
+        var temp16 = document.getElementById("temp16");
+        temp16.textContent = data.list[16].main.temp
+
+        var date24 = document.getElementById("date24");
+        date24.textContent = dayjs().add(4, 'day').format("MM/DD/YYYY")
+        var temp24 = document.getElementById("temp24");
+        temp16.textContent = data.list[24].main.temp
+
+        var date32 = document.getElementById("date32");
+        date32.textContent = dayjs().add(5, 'day').format("MM/DD/YYYY")
+        var temp32 = document.getElementById("temp32");
+        temp16.textContent = data.list[32].main.temp
+
+        
+
     })
 }
 
